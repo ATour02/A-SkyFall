4364481761a31bcfcb2728f37011c3be423179b0
sasha flory <sashaflory90@gmail.com>
2023-06-13 20:47:53 -0300

    punto referencial select

--- assets/scenes/Select.js
+++ assets/scenes/Select.js
@@ -1,10 +1,10 @@
-export default class Next extends Phaser.Scene {
+export default class Select extends Phaser.Scene {
     constructor() {
       super("Select");
     }
   // simular boton en imagen
     init() {
-     
+     this.score = 0;
     }
   
     preload() {
@@ -14,6 +14,9 @@ export default class Next extends Phaser.Scene {
     create() {
       
       this.add.image(400,300,"select").setScale(0.5);
+      this.ScoreText = this.add.text(365, 82, "8", {
+        fontSize: "50px",
+      });
 
     }
   
